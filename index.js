242a5c9a9bf15ae1007cc42993d7d00017fda3bc
rjxune <151636006+rjxune@users.noreply.github.com>
2024-04-01 11:08:38 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//para macall ung function
+document.querySelector("button").addEventListener("click",soundCheck);
+//function
+function soundCheck(){
+  let r1Sound = new Audio("sounds/r1-sound.mp3");
+   r1Sound.play();
+
+}  
+
